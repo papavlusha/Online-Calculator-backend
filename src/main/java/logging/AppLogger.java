8a97a73b89b96d10e6ad374fb3c4b5b3ce41577d
sva4rd <egor71175@gmail.com>
2024-03-03 12:53:09 +0300

    add logger

---
+++ src/main/java/logging/AppLogger.java
@@ -0,0 +1,17 @@
+package logging;
+
+import org.apache.logging.log4j.LogManager;
+
+public class AppLogger {
+    public static void logError(Class<?> className, String message) {
+        LogManager.getLogger(className).error(message);
+    }
+
+    public static void logWarn(Class<?> className, String message) {
+        LogManager.getLogger(className).warn(message);
+    }
+
+    public static void logInfo(Class<?> className, String message) {
+        LogManager.getLogger(className).info(message);
+    }
+}
