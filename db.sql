5a239e31947368e94aedfbef3656dfdb5fdf1974
sva4rd <egor71175@gmail.com>
2024-03-10 13:22:55 +0300

    upd data types and names

--- db.sql
+++ db.sql
@@ -1,56 +1,131 @@
--- Создание базы данных
-CREATE DATABASE IF NOT EXISTS calculator_db;
+-- MySQL dump 10.13  Distrib 8.0.34, for Win64 (x86_64)
+--
+-- Host: localhost    Database: calculator_db
+-- ------------------------------------------------------
+-- Server version	8.0.34
 
--- Использование созданной базы данных
-USE calculator_db;
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
--- Создание таблицы "Пользователи"
-CREATE TABLE IF NOT EXISTS Users (
-  user_id INT AUTO_INCREMENT PRIMARY KEY,
-  login VARCHAR(255) NOT NULL UNIQUE,
-  username VARCHAR(255),
-  email VARCHAR(255) NOT NULL,
-  password VARCHAR(255) NOT NULL,
-  is_admin BOOLEAN NOT NULL DEFAULT FALSE,
-  is_blocked BOOLEAN NOT NULL DEFAULT FALSE,
-  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-);
+--
+-- Current Database: `calculator_db`
+--
 
--- Создание таблицы "Чаты"
-CREATE TABLE IF NOT EXISTS Chats (
-  chat_id INT AUTO_INCREMENT PRIMARY KEY,
-  chatname VARCHAR(255) NOT NULL DEFAULT 'new chat',
-  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-);
+CREATE DATABASE /*!32312 IF NOT EXISTS*/ `calculator_db` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
 
--- Создание таблицы "Участники чата"
-CREATE TABLE IF NOT EXISTS ChatMembers (
-  chat_id INT NOT NULL,
-  user_id INT NOT NULL,
-  is_admin BOOLEAN NOT NULL DEFAULT FALSE,
-  is_blocked BOOLEAN NOT NULL DEFAULT FALSE,
-  FOREIGN KEY (chat_id) REFERENCES Chats(chat_id),
-  FOREIGN KEY (user_id) REFERENCES Users(user_id),
-  PRIMARY KEY (chat_id, user_id)
-);
+USE `calculator_db`;
 
--- Создание таблицы "Сообщения"
-CREATE TABLE IF NOT EXISTS Messages (
-  message_id INT AUTO_INCREMENT PRIMARY KEY,
-  chat_id INT NOT NULL,
-  user_id INT NOT NULL,
-  content TEXT NOT NULL,
-  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-  FOREIGN KEY (chat_id) REFERENCES Chats(chat_id),
-  FOREIGN KEY (user_id) REFERENCES Users(user_id)
-);
+--
+-- Table structure for table `chatmembers`
+--
 
--- Создание таблицы "Примеры"
-CREATE TABLE IF NOT EXISTS Examples (
-  example_id INT AUTO_INCREMENT PRIMARY KEY,
-  user_id INT NOT NULL,
-  content TEXT NOT NULL,
-  result TEXT NOT NULL,
-  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-  FOREIGN KEY (user_id) REFERENCES Users(user_id)
-);
+DROP TABLE IF EXISTS `chatmembers`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `chatmembers` (
+  `chat_id` bigint NOT NULL,
+  `user_id` bigint NOT NULL,
+  `is_admin_chat` tinyint(1) NOT NULL DEFAULT '0',
+  `is_blocked_chat` tinyint(1) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`chat_id`,`user_id`),
+  KEY `user_id` (`user_id`),
+  CONSTRAINT `chatmembers_ibfk_1` FOREIGN KEY (`chat_id`) REFERENCES `chats` (`chat_id`),
+  CONSTRAINT `chatmembers_ibfk_2` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `chats`
+--
+
+DROP TABLE IF EXISTS `chats`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `chats` (
+  `chat_id` bigint NOT NULL AUTO_INCREMENT,
+  `chatname` varchar(255) NOT NULL DEFAULT 'new chat',
+  `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`chat_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `examples`
+--
+
+DROP TABLE IF EXISTS `examples`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `examples` (
+  `example_id` bigint NOT NULL AUTO_INCREMENT,
+  `user_id` bigint NOT NULL,
+  `content` text NOT NULL,
+  `result` text NOT NULL,
+  `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`example_id`),
+  KEY `user_id` (`user_id`),
+  CONSTRAINT `examples_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `messages`
+--
+
+DROP TABLE IF EXISTS `messages`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `messages` (
+  `message_id` bigint NOT NULL AUTO_INCREMENT,
+  `chat_id` bigint NOT NULL,
+  `user_id` bigint NOT NULL,
+  `content` text NOT NULL,
+  `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`message_id`),
+  KEY `chat_id` (`chat_id`),
+  KEY `user_id` (`user_id`),
+  CONSTRAINT `messages_ibfk_1` FOREIGN KEY (`chat_id`) REFERENCES `chats` (`chat_id`),
+  CONSTRAINT `messages_ibfk_2` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `users` (
+  `user_id` bigint NOT NULL AUTO_INCREMENT,
+  `login` varchar(255) NOT NULL,
+  `username` varchar(255) DEFAULT NULL,
+  `email` varchar(255) NOT NULL,
+  `user_password` varchar(255) NOT NULL,
+  `is_admin` tinyint(1) NOT NULL DEFAULT '0',
+  `is_blocked` tinyint(1) NOT NULL DEFAULT '0',
+  `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`user_id`),
+  UNIQUE KEY `login` (`login`),
+  UNIQUE KEY `email` (`email`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2024-03-10 13:14:48
