af34608b41f9dc41fafcb07482560f648e75433e
sva4rd <egor71175@gmail.com>
2024-03-21 11:23:17 +0300

    upd db tables

--- calculator_db.sql
+++ calculator_db.sql
@@ -24,40 +24,6 @@ CREATE DATABASE /*!32312 IF NOT EXISTS*/ `calculator_db` /*!40100 DEFAULT CHARAC
 USE `calculator_db`;
 
 --
--- Table structure for table `chatmembers`
---
-
-DROP TABLE IF EXISTS `chatmembers`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `chatmembers` (
-  `chat_id` bigint NOT NULL,
-  `user_id` bigint NOT NULL,
-  `is_admin_chat` tinyint(1) NOT NULL DEFAULT '0',
-  `is_blocked_chat` tinyint(1) NOT NULL DEFAULT '0',
-  PRIMARY KEY (`chat_id`,`user_id`),
-  KEY `user_id` (`user_id`),
-  CONSTRAINT `chatmembers_ibfk_1` FOREIGN KEY (`chat_id`) REFERENCES `chats` (`chat_id`),
-  CONSTRAINT `chatmembers_ibfk_2` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Table structure for table `chats`
---
-
-DROP TABLE IF EXISTS `chats`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `chats` (
-  `chat_id` bigint NOT NULL AUTO_INCREMENT,
-  `chat_name` varchar(255) NOT NULL DEFAULT 'new chat',
-  `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
-  PRIMARY KEY (`chat_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
 -- Table structure for table `examples`
 --
 
@@ -69,7 +35,7 @@ CREATE TABLE `examples` (
   `user_id` bigint NOT NULL,
   `content` text NOT NULL,
   `result` text NOT NULL,
-  `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`example_id`),
   KEY `user_id` (`user_id`),
   CONSTRAINT `examples_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`)
@@ -85,15 +51,12 @@ DROP TABLE IF EXISTS `messages`;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `messages` (
   `message_id` bigint NOT NULL AUTO_INCREMENT,
-  `chat_id` bigint NOT NULL,
   `user_id` bigint NOT NULL,
   `content` text NOT NULL,
-  `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`message_id`),
-  KEY `chat_id` (`chat_id`),
   KEY `user_id` (`user_id`),
-  CONSTRAINT `messages_ibfk_1` FOREIGN KEY (`chat_id`) REFERENCES `chats` (`chat_id`),
-  CONSTRAINT `messages_ibfk_2` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`)
+  CONSTRAINT `messages_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -107,16 +70,21 @@ DROP TABLE IF EXISTS `users`;
 CREATE TABLE `users` (
   `user_id` bigint NOT NULL AUTO_INCREMENT,
   `login` varchar(255) NOT NULL,
-  `username` varchar(255) DEFAULT NULL,
   `email` varchar(255) NOT NULL,
   `user_password` varchar(255) NOT NULL,
+  `username` varchar(255) DEFAULT NULL,
+  `gender` varchar(255) DEFAULT NULL,
+  `about` text,
+  `solved_tasks` int DEFAULT '0',
+  `photo` blob,
+  `last_activity` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `is_admin` tinyint(1) NOT NULL DEFAULT '0',
   `is_blocked` tinyint(1) NOT NULL DEFAULT '0',
-  `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`user_id`),
   UNIQUE KEY `login` (`login`),
   UNIQUE KEY `email` (`email`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
@@ -128,4 +96,4 @@ CREATE TABLE `users` (
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2024-03-13 18:51:24
+-- Dump completed on 2024-03-21 11:18:04
