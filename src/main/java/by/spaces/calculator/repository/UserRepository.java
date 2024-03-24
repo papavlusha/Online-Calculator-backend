123fe89e2672f8ad8b67e3aeb4ebff6e01706f6a
sva4rd <egor71175@gmail.com>
2024-03-24 16:31:02 +0300

    add user rep

---
+++ src/main/java/by/spaces/calculator/repository/UserRepository.java
@@ -0,0 +1,25 @@
+package by.spaces.calculator.repository;
+
+import by.spaces.calculator.model.User;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Repository
+public interface UserRepository extends CrudRepository<User, Long> {
+    @Transactional
+    User findUserByUserId(Long id);
+
+    @Transactional
+    User findUserByLogin(String login);
+
+    @Transactional
+    List<User> findUsersByAdminTrue();
+
+    @Transactional
+    default User saveAndReturnUser(User user) {
+        return save(user);
+    }
+}
