e8bffd3d7b362d4b9a5b2d9273193841a5ca2ead
sva4rd <egor71175@gmail.com>
2024-03-24 16:32:17 +0300

    add app file

---
+++ src/main/java/by/spaces/calculator/Main.java
@@ -0,0 +1,11 @@
+package by.spaces.calculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication(scanBasePackages = "by.spaces.calculator")
+public class Main {
+    public static void main(String[] args)  {
+        SpringApplication.run(Main.class, args);
+    }
+}
