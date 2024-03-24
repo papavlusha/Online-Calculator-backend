2e17d516d2596e02b201b73c45e0da21256fb120
sva4rd <egor71175@gmail.com>
2024-03-24 17:20:42 +0300

    add req containers

---
+++ src/main/java/by/spaces/calculator/containers/SignUpRequest.java
@@ -0,0 +1,27 @@
+package by.spaces.calculator.containers;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+
+@Data
+public class SignUpRequest {
+
+    @NotBlank
+    @Size(min = 3, max = 40)
+    private String login;
+
+    @NotBlank
+    @Size(min = 3, max = 15)
+    private String username;
+
+    @NotBlank
+    @Size(max = 40)
+    @Email
+    private String email;
+
+    @NotBlank
+    @Size(min = 6, max = 20)
+    private String password;
+}
