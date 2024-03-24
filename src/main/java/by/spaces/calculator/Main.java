2fa4ec7d1aab0cd8feb1d9278552593f83a7ad3f
sva4rd <egor71175@gmail.com>
2024-03-25 00:18:00 +0300

    upd cors

--- src/main/java/by/spaces/calculator/Main.java
+++ src/main/java/by/spaces/calculator/Main.java
@@ -2,10 +2,25 @@ package by.spaces.calculator;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication(scanBasePackages = "by.spaces.calculator")
 public class Main {
     public static void main(String[] args)  {
         SpringApplication.run(Main.class, args);
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(    CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("http://localhost:4200")
+                        .allowedMethods("*");
+            }
+        };
     }
 }
