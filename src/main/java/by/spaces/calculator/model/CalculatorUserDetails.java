ea2868e2dd808d87b1665f6d4721ee66e4f82c8e
sva4rd <egor71175@gmail.com>
2024-03-25 00:17:00 +0300

    add calc user details

---
+++ src/main/java/by/spaces/calculator/model/CalculatorUserDetails.java
@@ -0,0 +1,47 @@
+package by.spaces.calculator.model;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+
+public class CalculatorUserDetails extends User implements UserDetails {
+    public CalculatorUserDetails(User user) {
+        super(user);
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @Override
+    public String getPassword() {
+        return super.getUserPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return super.getLogin();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
