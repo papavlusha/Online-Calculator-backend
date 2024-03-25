802b99066ac39e0faef3921e52b1bd1d8620ab1b
sva4rd <egor71175@gmail.com>
2024-03-25 09:56:03 +0300

    add models with reps

---
+++ src/main/java/by/spaces/calculator/model/Message.java
@@ -0,0 +1,54 @@
+package by.spaces.calculator.model;
+
+import jakarta.persistence.*;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.sql.Timestamp;
+
+@Entity
+@Table(name = "messages")
+@Builder(toBuilder = true)
+public class Message {
+    @Getter
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "message_id")
+    private Long messageId;
+
+    @Getter
+    @Setter
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @Getter
+    @Setter
+    @Column(name = "content", nullable = false)
+    private String content;
+
+    @Column(name = "created_at")
+    private Timestamp createdAt;
+
+    protected Message() {
+        //Empty
+    }
+
+    public static class MessageBuilder {
+        public Message build() {
+            if (user == null || content == null) {
+                throw new IllegalArgumentException("User and content must be provided");
+            }
+            Message mes = new Message();
+            mes.setUser(user);
+            mes.setContent(content);
+            mes.setCreatedAt(new Timestamp(System.currentTimeMillis()));
+            return mes;
+        }
+    }
+
+    public void setCreatedAt(Timestamp time) {
+        this.createdAt = new Timestamp(time.getTime());
+    }
+}
---
+++ src/main/java/by/spaces/calculator/repository/MessageRepository.java
@@ -0,0 +1,16 @@
+package by.spaces.calculator.repository;
+
+import by.spaces.calculator.model.Message;
+import by.spaces.calculator.model.User;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+@Repository
+public interface MessageRepository extends CrudRepository<Message, Long> {
+    @Transactional
+    Message findMessageByMessageId(Long id);
+
+    @Transactional
+    Message findMessagesByUser(User u);
+}
