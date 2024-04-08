fc5d414ed98280d37a17c43d5e83fe38d86739d1
sva4rd <egor71175@gmail.com>
2024-04-09 00:25:28 +0300

    upd Converter.dll connection

--- src/main/java/by/spaces/calculator/calculations/Converter.java
+++ src/main/java/by/spaces/calculator/calculations/Converter.java
@@ -1,9 +1,30 @@
 package by.spaces.calculator.calculations;
 
+import java.io.*;
+
 public class Converter {
     static {
-        System.loadLibrary("Сonverter");
+        try {
+            System.load(extractLibrary("Converter"));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
+
+    private static String extractLibrary(String name) throws IOException {
+        File tempDir = new File(System.getProperty("java.io.tmpdir"));
+        File tempLib = File.createTempFile(name, ".dll", tempDir);
+        try (InputStream in = Converter.class.getResourceAsStream("/native/" + System.mapLibraryName(name));
+             OutputStream out = new FileOutputStream(tempLib)) {
+            byte[] buffer = new byte[8192];
+            int bytesRead;
+            while ((bytesRead = in.read(buffer)) != -1) {
+                out.write(buffer, 0, bytesRead);
+            }
+        }
+        return tempLib.getAbsolutePath();
+    }
+
 
     public native String convertBase(String number, int sourceBase, int targetBase);
     public native boolean validateNumber(String number, int base);
