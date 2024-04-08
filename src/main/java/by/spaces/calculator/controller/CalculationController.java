0f1cff2f25d55cbdcda2f927a2ad7b07ac4873a1
sva4rd <egor71175@gmail.com>
2024-04-08 15:32:05 +0300

    controller with testing

---
+++ src/main/java/by/spaces/calculator/controller/CalculationController.java
@@ -0,0 +1,9 @@
+package by.spaces.calculator.controller;
+
+import by.spaces.calculator.calculations.Converter;
+
+public class CalculationController {
+//    Converter con = new Converter();
+//    String s = con.convertBase("10", 10, 2);
+//    logInfo(AuthController.class, "Test convert: " + s);
+}
