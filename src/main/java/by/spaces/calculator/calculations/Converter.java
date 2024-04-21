7331c287f3074d42ce1d8c714092f1d3e5646ee4
sva4rd <egor71175@gmail.com>
2024-04-21 14:54:33 +0300

    dll extraction -> separate class

--- src/main/java/by/spaces/calculator/calculations/Converter.java
+++ src/main/java/by/spaces/calculator/calculations/Converter.java
@@ -2,6 +2,8 @@ package by.spaces.calculator.calculations;
 
 import java.io.*;
 
+import static by.spaces.calculator.calculations.ExtractLibClass.extractLibrary;
+
 public class Converter {
     static {
         try {
@@ -10,21 +12,6 @@ public class Converter {
             throw new RuntimeException(e);
         }
     }
-
-    private static String extractLibrary(String name) throws IOException {
-        File tempDir = new File(System.getProperty("java.io.tmpdir"));
-        File tempLib = File.createTempFile(name, ".dll", tempDir);
-        try (InputStream in = Converter.class.getResourceAsStream("/native/" + System.mapLibraryName(name));
-             OutputStream out = new FileOutputStream(tempLib)) {
-            byte[] buffer = new byte[8192];
-            int bytesRead;
-            while ((bytesRead = in.read(buffer)) != -1) {
-                out.write(buffer, 0, bytesRead);
-            }
-        }
-        return tempLib.getAbsolutePath();
-    }
-
 
     public native String convertBase(String number, int sourceBase, int targetBase);
     public native boolean validateNumber(String number, int base);
--- src/main/java/by/spaces/calculator/calculations/Matrix.java
+++ src/main/java/by/spaces/calculator/calculations/Matrix.java
@@ -2,6 +2,8 @@ package by.spaces.calculator.calculations;
 
 import java.io.*;
 
+import static by.spaces.calculator.calculations.ExtractLibClass.extractLibrary;
+
 public class Matrix {
     static {
         try {
@@ -9,20 +11,6 @@ public class Matrix {
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
-    }
-
-    private static String extractLibrary(String name) throws IOException {
-        File tempDir = new File(System.getProperty("java.io.tmpdir"));
-        File tempLib = File.createTempFile(name, ".dll", tempDir);
-        try (InputStream in = Converter.class.getResourceAsStream("/native/" + System.mapLibraryName(name));
-             OutputStream out = new FileOutputStream(tempLib)) {
-            byte[] buffer = new byte[8192];
-            int bytesRead;
-            while ((bytesRead = in.read(buffer)) != -1) {
-                out.write(buffer, 0, bytesRead);
-            }
-        }
-        return tempLib.getAbsolutePath();
     }
 
     private long nativeHandle;
