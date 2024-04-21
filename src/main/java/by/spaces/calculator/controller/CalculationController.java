e3c5b0bd4ce382affb19eb68ea1183d3b9ef162e
sva4rd <egor71175@gmail.com>
2024-04-21 17:59:32 +0300

    upd with swagger annotation

--- src/main/java/by/spaces/calculator/controller/CalculationController.java
+++ src/main/java/by/spaces/calculator/controller/CalculationController.java
@@ -38,7 +38,7 @@ public class CalculationController {
     public ResponseEntity<PrimeCountResponse> calculatePrimes(@Valid @RequestBody PrimeCountRequest request) {
         Integer max = request.getMax();
         Integer start = request.getStart();
-        Integer thredNum = request.getThreads();
+        Integer threadNum = request.getThreads();
         Integer cycle = request.getCycleParam();
 
         String[] results = new String[2];
@@ -50,16 +50,16 @@ public class CalculationController {
                     .body(new PrimeCountResponse(null, null));
         }
 
-        if (start != null && thredNum != null && cycle != null) {
+        if (start != null && threadNum != null && cycle != null) {
             try {
-                results = new PrimeNumbersCount(start, max, thredNum, cycle).calculatePrimeCount();
+                results = new PrimeNumbersCount(start, max, threadNum, cycle).calculatePrimeCount();
             } catch (ExecutionException | InterruptedException e) {
                 logError(CalculationController.class, e.getMessage());
                 Thread.currentThread().interrupt();
             }
-        } else if (start != null && thredNum != null) {
+        } else if (start != null && threadNum != null) {
             try {
-                results = new PrimeNumbersCount(start, max, thredNum).calculatePrimeCount();
+                results = new PrimeNumbersCount(start, max, threadNum).calculatePrimeCount();
             } catch (ExecutionException | InterruptedException e) {
                 logError(CalculationController.class, e.getMessage());
                 Thread.currentThread().interrupt();
@@ -90,6 +90,11 @@ public class CalculationController {
 
     private final CalculationService service = new CalculationService();
 
+    @Operation(summary = "Number conversion", description = "This method Convert numbers to binary, decimal, octal and hexadecimal number systems.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Convert number successfully"),
+            @ApiResponse(responseCode = "403", description = "Conversion failed")
+    })
     @PostMapping("/converter")
     public ResponseEntity<ConvertResponse> convertNumber(@Valid @RequestBody ConvertRequest convertRequest) {
         int sourceBase = Integer.parseInt(convertRequest.getSourceBase());
@@ -98,47 +103,85 @@ public class CalculationController {
         String dec = service.convertNumber(number, sourceBase, 10);
         String oct = service.convertNumber(number, sourceBase, 8);
         String hex = service.convertNumber(number, sourceBase, 16);
-
+        logInfo(CalculationController.class, "Get result: bin - " + bin
+                + ", dec - " + dec
+                + ", oct - " + oct
+                + ", hex - " + hex);
         ConvertResponse response = new ConvertResponse(bin, dec, oct, hex);
         return ResponseEntity.ok(response);
     }
 
+    @Operation(summary = "Matrix determinant", description = "Calculating the determinant of a matrix.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "The determinant has been calculated successfully"),
+            @ApiResponse(responseCode = "403", description = "Calculation of the determinant failed")
+    })
     @PostMapping("/matrix/determinant")
     public ResponseEntity<String> getMatrixDeterminant(@Valid @RequestBody Object requestData) {
         String determinant = String.valueOf(service.getDeterminant(requestData));
         return ResponseEntity.ok(determinant);
     }
 
+    @Operation(summary = "Inverse matrix", description = "Inverse matrix calculation.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Inverse matrix calculated successfully"),
+            @ApiResponse(responseCode = "403", description = "Inverse matrix calculation failed")
+    })
     @PostMapping("/matrix/inverse")
     public ResponseEntity<double[][]> inverseMatrix(@Valid @RequestBody Object requestData) {
         double[][] matrix = service.getInverseMatrix(requestData);
         return ResponseEntity.ok(matrix);
     }
 
+    @Operation(summary = "Transpose matrix", description = "Transpose matrix calculation.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Transpose matrix calculated successfully"),
+            @ApiResponse(responseCode = "403", description = "Transpose matrix calculation failed")
+    })
     @PostMapping("/matrix/transpose")
     public ResponseEntity<double[][]> transposeMatrix(@Valid @RequestBody Object requestData) {
         double[][] matrix = service.getTransposeMatrix(requestData);
         return ResponseEntity.ok(matrix);
     }
 
+    @Operation(summary = "Multiplication of a matrix by a scalar", description = "Calculating a matrix by multiplying the original matrix by a scalar.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Matrix multiplied by a scalar successfully"),
+            @ApiResponse(responseCode = "403", description = "Matrix multiplication by scalar failed")
+    })
     @PostMapping("/matrix/multiply_by_scalar")
     public ResponseEntity<double[][]> multiplyMatrixByScalar(@Valid @RequestBody MatrixWithScalarReq requestData) {
         double[][] matrix = service.matrixMultiplyByScalar(requestData.getMatrixData(), requestData.getScalar());
         return ResponseEntity.ok(matrix);
     }
 
+    @Operation(summary = "Matrix addition", description = "Adding two matrices of the same size.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Matrix addition was successful"),
+            @ApiResponse(responseCode = "403", description = "Matrix addition failed")
+    })
     @PostMapping("/matrix/add")
     public ResponseEntity<double[][]> addMatrix(@Valid @RequestBody MatricesRequest requestData) {
         double[][] matrix = service.matrixAddition(requestData.getMatrixData(), requestData.getMatrixData2());
         return ResponseEntity.ok(matrix);
     }
 
+    @Operation(summary = "Matrix subtraction", description = "Subtraction two matrices of the same size.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Matrix subtraction was successful"),
+            @ApiResponse(responseCode = "403", description = "Matrix subtraction failed")
+    })
     @PostMapping("/matrix/subtract")
     public ResponseEntity<double[][]> subtractMatrix(@Valid @RequestBody MatricesRequest requestData) {
         double[][] matrix = service.matrixSubtraction(requestData.getMatrixData(), requestData.getMatrixData2());
         return ResponseEntity.ok(matrix);
     }
 
+    @Operation(summary = "Matrix multiplication", description = "Multiplication two matrices.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Matrix multiplication was successful"),
+            @ApiResponse(responseCode = "403", description = "Matrix multiplication failed")
+    })
     @PostMapping("/matrix/multiply")
     public ResponseEntity<double[][]> multiplyMatrix(@Valid @RequestBody MatricesRequest requestData) {
         double[][] matrix = service.matrixMultiplication(requestData.getMatrixData(), requestData.getMatrixData2());
