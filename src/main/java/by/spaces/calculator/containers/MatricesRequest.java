ede41ce647f249020eca5064cf625d7b1691a3dd
sva4rd <egor71175@gmail.com>
2024-04-21 21:22:26 +0300

    upd containers and dependencies

--- src/main/java/by/spaces/calculator/containers/MatricesRequest.java
+++ src/main/java/by/spaces/calculator/containers/MatricesRequest.java
@@ -1,17 +1,17 @@
 package by.spaces.calculator.containers;
 
 import io.swagger.v3.oas.annotations.media.Schema;
-import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
 @Data
 @Schema(description = "Container with two matrices")
 public class MatricesRequest {
     @Schema(description = "First matrix data", example = "2 2\n1 2\n3 4")
-    @NotBlank
+    @NotNull
     private Object matrixData;
 
     @Schema(description = "Second matrix data", example = "[[1, 2], [3, 4]]")
-    @NotBlank
+    @NotNull
     private Object matrixData2;
 }
--- src/main/java/by/spaces/calculator/containers/MatrixWithScalarReq.java
+++ src/main/java/by/spaces/calculator/containers/MatrixWithScalarReq.java
@@ -2,13 +2,14 @@ package by.spaces.calculator.containers;
 
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
 @Data
 @Schema(description = "Container with matrix and scalar")
 public class MatrixWithScalarReq {
     @Schema(description = "Matrix data", example = "2 2\n1 2\n3 4")
-    @NotBlank
+    @NotNull
     private Object matrixData;
 
     @Schema(description = "Scalar", example = "5")
