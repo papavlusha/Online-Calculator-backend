f86e779b0cb4a461fcd21c7844c0d59875121c4a
sva4rd <egor71175@gmail.com>
2024-04-21 21:23:07 +0300

    upd logs, errors and annotations in calculations

--- src/main/java/by/spaces/calculator/controller/CalculationController.java
+++ src/main/java/by/spaces/calculator/controller/CalculationController.java
@@ -1,15 +1,13 @@
 package by.spaces.calculator.controller;
 
-import by.spaces.calculator.containers.PrimeCountRequest;
-import by.spaces.calculator.containers.PrimeCountResponse;
+import by.spaces.calculator.containers.*;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.ExampleObject;
+import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import by.spaces.calculator.containers.ConvertRequest;
-import by.spaces.calculator.containers.ConvertResponse;
-import by.spaces.calculator.containers.MatricesRequest;
-import by.spaces.calculator.containers.MatrixWithScalarReq;
 import by.spaces.calculator.service.CalculationService;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
@@ -17,11 +15,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
-import java.net.URI;
 import java.util.Arrays;
-import java.util.concurrent.ExecutionException;
 
 import static by.spaces.calculator.logging.AppLogger.logError;
 import static by.spaces.calculator.logging.AppLogger.logInfo;
@@ -42,7 +37,7 @@ public class CalculationController {
         Integer cycle = request.getCycleParam();
         try{
             String[] results = service.calculatePrimes(start, max, threadNum, cycle);
-            logInfo(CalculationController.class, "Get result: " + Arrays.toString(results));
+            logInfo(CalculationController.class, "Get primes result: " + Arrays.toString(results));
             return ResponseEntity.ok(new PrimeCountResponse(results[0], results[1], null));
         } catch (Exception e){
             return handleException(e);
@@ -75,7 +70,7 @@ public class CalculationController {
             String dec = service.convertNumber(number, sourceBase, 10);
             String oct = service.convertNumber(number, sourceBase, 8);
             String hex = service.convertNumber(number, sourceBase, 16);
-            logInfo(CalculationController.class, "Get result: bin - " + bin
+            logInfo(CalculationController.class, "Get convert result: bin - " + bin
                     + ", dec - " + dec
                     + ", oct - " + oct
                     + ", hex - " + hex);
@@ -91,13 +86,22 @@ public class CalculationController {
 
     @Operation(summary = "Matrix determinant", description = "Calculating the determinant of a matrix.")
     @ApiResponses(value = {
-            @ApiResponse(responseCode = "200", description = "The determinant has been calculated successfully"),
-            @ApiResponse(responseCode = "400", description = "Calculation of the determinant failed")
+            @ApiResponse(responseCode = "200", description = "The determinant has been calculated successfully",
+                    content = @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = String.class),
+                            examples = @ExampleObject(value = "25"))),
+            @ApiResponse(responseCode = "400", description = "Calculation of the determinant failed",
+                    content = @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = String.class),
+                            examples = @ExampleObject(value = "Determinant calculation failed: Wrong matrix string")))
     })
     @PostMapping("/matrix/determinant")
-    public ResponseEntity<String> getMatrixDeterminant(@Valid @RequestBody Object requestData) {
+    public ResponseEntity<String> getMatrixDeterminant(@Valid @RequestBody
+                                                           @Schema(example = "[[1,2],[3,4]]")
+                                                           Object requestData) {
         try{
             String determinant = String.valueOf(service.getDeterminant(requestData));
+            logInfo(CalculationController.class, "Get determinant result: " + determinant);
             return ResponseEntity.ok(determinant);
         } catch (Exception e){
             String error = "Determinant calculation failed: " + e.getMessage();
@@ -109,66 +113,108 @@ public class CalculationController {
     @Operation(summary = "Inverse matrix", description = "Inverse matrix calculation.")
     @ApiResponses(value = {
             @ApiResponse(responseCode = "200", description = "Inverse matrix calculated successfully"),
-            @ApiResponse(responseCode = "403", description = "Inverse matrix calculation failed")
+            @ApiResponse(responseCode = "400", description = "Inverse matrix calculation failed")
     })
     @PostMapping("/matrix/inverse")
-    public ResponseEntity<double[][]> inverseMatrix(@Valid @RequestBody Object requestData) {
-        double[][] matrix = service.getInverseMatrix(requestData);
-        return ResponseEntity.ok(matrix);
+    public ResponseEntity<MatrixResponse> inverseMatrix(@Valid @RequestBody
+                                                            @Schema(example = "[[1,2],[3,4]]")
+                                                            Object requestData) {
+        try{
+            double[][] matrix = service.getInverseMatrix(requestData);
+            logInfo(CalculationController.class, "Get inverse result: " + Arrays.toString(matrix));
+            return ResponseEntity.ok(new MatrixResponse(matrix, null));
+        } catch (Exception e){
+            return handleMatrixException("Inverse matrix calculation failed: ", e);
+        }
     }
 
     @Operation(summary = "Transpose matrix", description = "Transpose matrix calculation.")
     @ApiResponses(value = {
             @ApiResponse(responseCode = "200", description = "Transpose matrix calculated successfully"),
-            @ApiResponse(responseCode = "403", description = "Transpose matrix calculation failed")
+            @ApiResponse(responseCode = "400", description = "Transpose matrix calculation failed")
     })
     @PostMapping("/matrix/transpose")
-    public ResponseEntity<double[][]> transposeMatrix(@Valid @RequestBody Object requestData) {
-        double[][] matrix = service.getTransposeMatrix(requestData);
-        return ResponseEntity.ok(matrix);
+    public ResponseEntity<MatrixResponse> transposeMatrix(@Valid @RequestBody
+                                                              @Schema(example = "[[1,2],[3,4]]")
+                                                              Object requestData) {
+        try{
+            double[][] matrix = service.getTransposeMatrix(requestData);
+            logInfo(CalculationController.class, "Get transpose result: " + Arrays.toString(matrix));
+            return ResponseEntity.ok(new MatrixResponse(matrix, null));
+        } catch (Exception e){
+            return handleMatrixException("Transpose matrix calculation failed: ", e);
+        }
     }
 
     @Operation(summary = "Multiplication of a matrix by a scalar", description = "Calculating a matrix by multiplying the original matrix by a scalar.")
     @ApiResponses(value = {
             @ApiResponse(responseCode = "200", description = "Matrix multiplied by a scalar successfully"),
-            @ApiResponse(responseCode = "403", description = "Matrix multiplication by scalar failed")
+            @ApiResponse(responseCode = "400", description = "Matrix multiplication by scalar failed")
     })
     @PostMapping("/matrix/multiply_by_scalar")
-    public ResponseEntity<double[][]> multiplyMatrixByScalar(@Valid @RequestBody MatrixWithScalarReq requestData) {
-        double[][] matrix = service.matrixMultiplyByScalar(requestData.getMatrixData(), requestData.getScalar());
-        return ResponseEntity.ok(matrix);
+    public ResponseEntity<MatrixResponse> multiplyMatrixByScalar(@Valid @RequestBody MatrixWithScalarReq requestData) {
+        try{
+            double[][] matrix = service.matrixMultiplyByScalar(requestData.getMatrixData(), requestData.getScalar());
+            logInfo(CalculationController.class, "Get scalar multiply result: " + Arrays.toString(matrix));
+            return ResponseEntity.ok(new MatrixResponse(matrix, null));
+        } catch (Exception e){
+            return handleMatrixException("Matrix multiplication by scalar failed: ", e);
+        }
     }
 
     @Operation(summary = "Matrix addition", description = "Adding two matrices of the same size.")
     @ApiResponses(value = {
             @ApiResponse(responseCode = "200", description = "Matrix addition was successful"),
-            @ApiResponse(responseCode = "403", description = "Matrix addition failed")
+            @ApiResponse(responseCode = "400", description = "Matrix addition failed")
     })
     @PostMapping("/matrix/add")
-    public ResponseEntity<double[][]> addMatrix(@Valid @RequestBody MatricesRequest requestData) {
-        double[][] matrix = service.matrixAddition(requestData.getMatrixData(), requestData.getMatrixData2());
-        return ResponseEntity.ok(matrix);
+    public ResponseEntity<MatrixResponse> addMatrix(@Valid @RequestBody MatricesRequest requestData) {
+        try{
+            double[][] matrix = service.matrixAddition(requestData.getMatrixData(), requestData.getMatrixData2());
+            logInfo(CalculationController.class, "Get matrix addition result: " + Arrays.toString(matrix));
+            return ResponseEntity.ok(new MatrixResponse(matrix, null));
+        } catch (Exception e){
+            return handleMatrixException("Matrix addition failed: ", e);
+        }
     }
 
     @Operation(summary = "Matrix subtraction", description = "Subtraction two matrices of the same size.")
     @ApiResponses(value = {
             @ApiResponse(responseCode = "200", description = "Matrix subtraction was successful"),
-            @ApiResponse(responseCode = "403", description = "Matrix subtraction failed")
+            @ApiResponse(responseCode = "400", description = "Matrix subtraction failed")
     })
     @PostMapping("/matrix/subtract")
-    public ResponseEntity<double[][]> subtractMatrix(@Valid @RequestBody MatricesRequest requestData) {
-        double[][] matrix = service.matrixSubtraction(requestData.getMatrixData(), requestData.getMatrixData2());
-        return ResponseEntity.ok(matrix);
+    public ResponseEntity<MatrixResponse> subtractMatrix(@Valid @RequestBody MatricesRequest requestData) {
+        try{
+            double[][] matrix = service.matrixSubtraction(requestData.getMatrixData(), requestData.getMatrixData2());
+            logInfo(CalculationController.class, "Get matrix subtraction result: " + Arrays.toString(matrix));
+            return ResponseEntity.ok(new MatrixResponse(matrix, null));
+        } catch (Exception e){
+            return handleMatrixException("Matrix subtraction failed: ", e);
+        }
     }
 
     @Operation(summary = "Matrix multiplication", description = "Multiplication two matrices.")
     @ApiResponses(value = {
             @ApiResponse(responseCode = "200", description = "Matrix multiplication was successful"),
-            @ApiResponse(responseCode = "403", description = "Matrix multiplication failed")
+            @ApiResponse(responseCode = "400", description = "Matrix multiplication failed")
     })
     @PostMapping("/matrix/multiply")
-    public ResponseEntity<double[][]> multiplyMatrix(@Valid @RequestBody MatricesRequest requestData) {
-        double[][] matrix = service.matrixMultiplication(requestData.getMatrixData(), requestData.getMatrixData2());
-        return ResponseEntity.ok(matrix);
+    public ResponseEntity<MatrixResponse> multiplyMatrix(@Valid @RequestBody MatricesRequest requestData) {
+        try{
+            double[][] matrix = service.matrixMultiplication(requestData.getMatrixData(), requestData.getMatrixData2());
+            logInfo(CalculationController.class, "Get matrix multiplication result: " + Arrays.toString(matrix));
+            return ResponseEntity.ok(new MatrixResponse(matrix, null));
+        } catch (Exception e){
+            return handleMatrixException("Matrix multiplication failed: ", e);
+        }
+    }
+
+    private ResponseEntity<MatrixResponse> handleMatrixException(String err, Exception e) {
+        String error = err + e.getMessage();
+        logError(CalculationController.class, error);
+        MatrixResponse errorResponse = new MatrixResponse();
+        errorResponse.setError(error);
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorResponse);
     }
 }
--- src/main/java/by/spaces/calculator/service/CalculationService.java
+++ src/main/java/by/spaces/calculator/service/CalculationService.java
@@ -6,6 +6,8 @@ import by.spaces.calculator.calculations.PrimeNumbersCount;
 import by.spaces.calculator.controller.CalculationController;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.Scanner;
 import java.util.concurrent.ExecutionException;
 
 import static by.spaces.calculator.logging.AppLogger.logError;
@@ -13,60 +15,102 @@ import static by.spaces.calculator.logging.AppLogger.logError;
 @Service
 public class CalculationService {
     private final Converter converter = new Converter();
-    public String convertNumber(String number, int sourceBase, int targetBase){
+
+    public String convertNumber(String number, int sourceBase, int targetBase) {
         if (sourceBase != 2 && sourceBase != 8 && sourceBase != 10 && sourceBase != 16)
             throw new IllegalArgumentException("Wrong sourceBase");
-        try{
+        try {
             double num = Double.parseDouble(number);
             if (num < 0)
                 throw new IllegalArgumentException();
-        }catch (Exception e){
+        } catch (Exception e) {
             throw new IllegalArgumentException("The number must be non-negative");
         }
         return converter.convertBase(number, sourceBase, targetBase);
     }
 
-    private Matrix createMatrix(Object matrixData){
+    private boolean validateMatrixData(String inputString) {
+        try (Scanner scanner = new Scanner(inputString)) {
+            int rows = scanner.nextInt();
+            int cols = scanner.nextInt();
+            if (rows <= 0 || cols <= 0)
+                return false;
+            int count = 0;
+            for (int i = 0; i < rows; i++) {
+                for (int j = 0; j < cols; j++) {
+                    if (!scanner.hasNextDouble())
+                        return false;
+                    scanner.nextDouble();
+                    count++;
+                }
+            }
+            if (scanner.hasNextDouble())
+                return false;
+            return count == rows * cols;
+        } catch (Exception ignored) {
+            return false;
+        }
+    }
+
+    private Matrix createMatrix(Object matrixData) {
         Matrix matrix;
         if (matrixData instanceof String data) {
+            if (!validateMatrixData(data))
+                throw new IllegalArgumentException("Wrong matrix string");
             matrix = new Matrix(data);
         } else if (matrixData instanceof double[][] data) {
             matrix = new Matrix(data);
+        } else if (matrixData instanceof ArrayList<?> data) {
+            if (!data.isEmpty() && data.get(0) instanceof ArrayList<?> && !((ArrayList<?>) data.get(0)).isEmpty()) {
+                double[][] array;
+                if (((ArrayList<?>) data.get(0)).get(0) instanceof Double)
+                    array = data.stream()
+                            .map(row -> ((ArrayList<Double>) row).stream().mapToDouble(Double::doubleValue).toArray())
+                            .toArray(double[][]::new);
+                else if (((ArrayList<?>) data.get(0)).get(0) instanceof Integer)
+                    array = data.stream()
+                            .map(row -> ((ArrayList<Integer>) row).stream().mapToDouble(Integer::doubleValue).toArray())
+                            .toArray(double[][]::new);
+                else
+                    throw new IllegalArgumentException("Wrong matrix array");
+                matrix = new Matrix(array);
+            } else
+                throw new IllegalArgumentException("Wrong matrix array");
         } else {
             throw new IllegalArgumentException("Wrong matrix data");
         }
         return matrix;
     }
 
-    public double getDeterminant(Object matrixData){
+    public double getDeterminant(Object matrixData) {
         return createMatrix(matrixData).findDeterminant();
     }
 
-    public double[][] getInverseMatrix(Object matrixData){
+    public double[][] getInverseMatrix(Object matrixData) {
         return createMatrix(matrixData).inverseMatrix().getData();
     }
 
-    public double[][] getTransposeMatrix(Object matrixData){
+    public double[][] getTransposeMatrix(Object matrixData) {
         return createMatrix(matrixData).transpose().getData();
     }
 
-    public double[][] matrixMultiplyByScalar(Object matrixData, String scalar){
+    public double[][] matrixMultiplyByScalar(Object matrixData, String scalar) {
         return createMatrix(matrixData).multiply(Double.parseDouble(scalar)).getData();
     }
 
-    public double[][] matrixAddition(Object matrixData, Object matrixData2){
+    public double[][] matrixAddition(Object matrixData, Object matrixData2) {
         return createMatrix(matrixData).add(createMatrix(matrixData2)).getData();
     }
 
-    public double[][] matrixSubtraction(Object matrixData, Object matrixData2){
+    public double[][] matrixSubtraction(Object matrixData, Object matrixData2) {
         return createMatrix(matrixData).subtract(createMatrix(matrixData2)).getData();
     }
 
-    public double[][] matrixMultiplication(Object matrixData, Object matrixData2){
+    public double[][] matrixMultiplication(Object matrixData, Object matrixData2) {
         return createMatrix(matrixData).multiply(createMatrix(matrixData2)).getData();
     }
 
-    private void checkRange(Integer start, Integer max){
+    private void checkRange(Integer start, Integer max) {
         if (max == null || max < 2)
             throw new IllegalArgumentException("Wrong parameter \"max\"");
 
