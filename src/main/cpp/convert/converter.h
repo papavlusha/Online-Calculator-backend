64e07c7a6fe146a4baa972b6111ff17241abb843
sva4rd <egor71175@gmail.com>
2024-04-21 21:24:14 +0300

    del unused cpp

--- src/main/cpp/convert/converter.h
+++
@@ -1,9 +0,0 @@
-#ifndef CONVERTER_H
-#define CONVERTER_H
-
-#include <string>
-#include <iostream>
-#include <cmath>
-std::string convertBase(const std::string& number, int sourceBase, int targetBase);
-bool validateNumber(const std::string& number, int base);
-#endif // CONVERTER_H
