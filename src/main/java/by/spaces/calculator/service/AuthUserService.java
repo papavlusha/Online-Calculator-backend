ab3377c8268c40bb3b8860b865f1a07c6dd6ea8e
sva4rd <egor71175@gmail.com>
2024-05-26 18:49:09 +0300

    Split UserService logic

---
+++ src/main/java/by/spaces/calculator/service/AuthUserService.java
@@ -0,0 +1,55 @@
+package by.spaces.calculator.service;
+
+import by.spaces.calculator.model.User;
+import by.spaces.calculator.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import static by.spaces.calculator.logging.AppLogger.*;
+
+@Service
+public class AuthUserService {
+
+    private final PasswordEncoder passwordEncoder;
+    private final UserRepository userRepository;
+    private final AuthenticationManager authenticationManager;
+    private final JwtService tokenProvider;
+
+    @Autowired
+    public AuthUserService(PasswordEncoder p, UserRepository u, AuthenticationManager a, JwtService j){
+        passwordEncoder = p;
+        userRepository = u;
+        authenticationManager = a;
+        tokenProvider = j;
+    }
+
+    public String loginUser(String username, String password) {
+        Authentication authentication = authenticationManager
+                .authenticate(new UsernamePasswordAuthenticationToken(username, password));
+        return tokenProvider.generateToken(authentication);
+    }
+
+    public void registerUser(User user) {
+        logInfo(AuthUserService.class, "registering user " + user.getUsername());
+
+        if(userRepository.existsByLogin(user.getLogin())) {
+            logWarn(AuthUserService.class, "login " + user.getLogin() + " already exists.");
+
+            throw new UsernameAlreadyExistsException(
+                    String.format("login %s already exists", user.getLogin()));
+        }
+
+        if(userRepository.existsByEmail(user.getEmail())) {
+            logWarn(AuthUserService.class, "email " + user.getEmail() + " already exists.");
+
+            throw new EmailAlreadyExistsException(
+                    String.format("email %s already exists", user.getEmail()));
+        }
+        user.setUserPassword(passwordEncoder.encode(user.getUserPassword()));
+        userRepository.save(user);
+    }
+}
